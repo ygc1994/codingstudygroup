dad3aba2ac4df7c83aa627ea2d4462d1203e7cb0
ygc1994 <ygc1994@naver.com>
2024-08-04 18:09:12 +0900

    Programmers_lv0

---
+++ src/programmers/level0/배열_뒤집기.java
@@ -0,0 +1,34 @@
+package programmers.level0;
+
+public class 배열_뒤집기 {
+
+    public static void main(String[] args) {
+
+        int[] num_list = new int [] {1, 0, 1, 1, 1, 3, 5};
+        int[] answer = {};
+        int num = 0;
+
+        answer = new int[num_list.length];
+
+        System.out.print('[');
+
+        for (int i = 0; i < num_list.length; i++) {
+            System.out.print(num_list[i] + ", ");
+        }
+
+        System.out.println("\b\b]");
+
+        for (int i = num_list.length - 1; i >= 0; i--) {
+            answer[num] = num_list[i];
+            num++;
+        }
+
+        System.out.print('[');
+
+        for (int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i] + ", ");
+        }
+
+        System.out.println("\b\b]");
+    }
+}
