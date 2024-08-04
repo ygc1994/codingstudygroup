6bfc977a21e0ba20153f194ac74fb507d23ec9c4
ygc1994 <ygc1994@naver.com>
2024-08-04 13:13:44 +0900

    Programmers_lv0

---
+++ src/programmers/level0/나이_출력.java
@@ -0,0 +1,16 @@
+package programmers.level0;
+
+public class 나이_출력 {
+
+    public static void main(String[] args) {
+
+        int age = 23;
+        int answer = 0;
+
+        answer = 2022 - age + 1;
+
+        System.out.println("2022년 기준 " + age + "살이므로 " + answer + "년생입니다.");
+
+    }
+
+}
