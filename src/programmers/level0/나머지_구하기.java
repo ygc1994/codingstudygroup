745680ee66020be88ed6cedfa7688cf740eea8d3
ygc1994 <ygc1994@naver.com>
2024-08-04 13:08:02 +0900

    Programmers_lv0

---
+++ src/programmers/level0/나머지_구하기.java
@@ -0,0 +1,18 @@
+package programmers.level0;
+
+public class 나머지_구하기 {
+
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        int num2 = 5;
+        int answer = -1;
+
+        answer = num1 % num2;
+
+        System.out.println("num1이 " + num1 + ", num2가 " + num2 + "이므로 " + num1 + "을 " + num2 + "로 나눈 나머지 " +
+                answer + "을 return 합니다.");
+
+    }
+
+}
--- src/programmers/level0/숫자_비교하기.java
+++ src/programmers/level0/숫자_비교하기.java
@@ -6,17 +6,17 @@ public class 숫자_비교하기 {
 
         int num1 = 7;
         int num2 = 99;
-        int result = 0;
+        int answer = 0;
 
         if(num1 == num2) {
-            result = 1;
+            answer = 1;
 
-            System.out.println("num1이 " + num1 + "이고 num2가 " + num2 + "이므로 같습니다. 따라서 " + result + "을 " +
+            System.out.println("num1이 " + num1 + "이고 num2가 " + num2 + "이므로 같습니다. 따라서 " + answer + "을 " +
                     "return합니다.");
         } else {
-            result = -1;
+            answer = -1;
 
-            System.out.println("num1이 " + num1 + "이고 num2가 " + num2 + "이므로 다릅니다. 따라서 " + result + "을 " +
+            System.out.println("num1이 " + num1 + "이고 num2가 " + num2 + "이므로 다릅니다. 따라서 " + answer + "을 " +
                     "return합니다.");
         }
 
