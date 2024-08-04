4f37b2db1e46427cbfd6b2298c85957c7b054771
ygc1994 <ygc1994@naver.com>
2024-08-04 13:16:17 +0900

    Programmers_lv0

---
+++ src/programmers/level0/몫_구하기.java
@@ -0,0 +1,18 @@
+package programmers.level0;
+
+public class 몫_구하기 {
+
+    public static void main(String[] args) {
+
+        int num1 = 7;
+        int num2 = 2;
+        int answer = 0;
+
+        answer = num1 / num2;
+
+        System.out.println("num1이 " + num1 + ", num2가 " + num2 + "이므로 " + num1 + "을 " + num2 + "로 나눈 몫 " +
+                answer + "을 return 합니다.");
+
+    }
+
+}
