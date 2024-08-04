d68a1817cce951f872967a7ad9c0ca7e9a85af66
ygc1994 <ygc1994@naver.com>
2024-08-04 18:16:46 +0900

    Programmers_lv0

---
+++ src/programmers/level0/편지.java
@@ -0,0 +1,17 @@
+package programmers.level0;
+
+public class 편지 {
+
+    public static void main(String[] args) {
+
+        String message = "I love you~";
+        int answer = 0;
+
+        answer = message.length() * 2;
+
+        System.out.println("message : " + '\"' + message + '\"');
+        System.out.println("result : " + answer);
+
+    }
+
+}
