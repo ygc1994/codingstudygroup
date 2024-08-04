525469d4f8e33415cb380bb68fb8368b51d655eb
ygc1994 <ygc1994@naver.com>
2024-08-04 13:57:17 +0900

    Programmers_lv0

---
+++ src/programmers/level0/두_수의_곱.java
@@ -0,0 +1,16 @@
+package programmers.level0;
+
+public class 두_수의_곱 {
+
+    public static void main(String[] args) {
+
+        int num1 = 27;
+        int num2 = 19;
+        int answer = 0;
+
+        answer = num1 * num2;
+
+        System.out.println("num1이 " + num1 + ", num2가 " + num2 + "이므로 " + num1 + " * " + num2 + " = " +
+                answer + "을 return합니다.");
+    }
+}
