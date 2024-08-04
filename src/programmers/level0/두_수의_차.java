a7a84fe4b09ea3be93e820910e405eff8f7bf14b
ygc1994 <ygc1994@naver.com>
2024-08-04 12:46:09 +0900

    Programmers_lv0

---
+++ src/programmers/level0/두_수의_차.java
@@ -0,0 +1,18 @@
+package programmers.level0;
+
+public class 두_수의_차 {
+
+    public static void main(String[] args) {
+
+        int num1 = 100;
+        int num2 = 2;
+        int answer = 0;
+
+        answer = num1 - num2;
+
+        System.out.println("num1이 " + num1 + "이고 num2가 " + num2 + "이므로 " + num1 + " - " + num2 + " = " + answer + "" +
+                "을 return합니다.");
+
+    }
+
+}
